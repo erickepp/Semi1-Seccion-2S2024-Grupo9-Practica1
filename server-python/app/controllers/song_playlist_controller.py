08f452b3316181cc4dda792ff441a88284831183
Erick Patzan <erickestuardopatzan@gmail.com>
2024-08-26 00:07:28 -0600

    Se agregaron los endpoints POST y DELETE para las canciones de las playlists

--- server-python/app/controllers/song_playlist_controller.py
+++ server-python/app/controllers/song_playlist_controller.py
@@ -1,7 +1,9 @@
-from flask import jsonify
+from flask import request, jsonify
 from app.models.song_playlist import SongPlaylist
 from app.models.playlist import Playlist
+from app.models.song import Song
 from app.models.user import User
+from config.db_config import db
 
 
 def get_songs_playlists_by_user(user_id):
@@ -51,5 +53,65 @@ def get_songs_playlist(playlist_id):
         playlist_dict['songs'] = [song.to_dict().get('song') for song in songs_playlist]
 
         return jsonify(playlist_dict), 200
+    except Exception as e:
+        return jsonify({'message': f'Error interno del servidor: {str(e)}'}), 500
+
+
+def register_song_playlist():
+    try:
+        data = request.json
+        song_id = data.get('song_id')
+        playlist_id = data.get('playlist_id')
+
+        # Validar los datos recibidos
+        if not song_id or not playlist_id:
+            return jsonify({'message': 'El song_id y el playlist_id son necesarios.'}), 400
+
+        # Verificar si la canción y la playlist existen
+        song = Song.query.get(song_id)
+        playlist = Playlist.query.get(playlist_id)
+
+        if not song:
+            return jsonify({'message': 'Canción no encontrada.'}), 404
+        if not playlist:
+            return jsonify({'message': 'Playlist no encontrada.'}), 404
+
+        # Verificar si la canción ya está en la playlist
+        existing_song_playlist = SongPlaylist.query.filter_by(song_id=song_id, playlist_id=playlist_id).first()
+        if existing_song_playlist:
+            return jsonify({'message': 'La canción ya está en la playlist.'}), 400
+
+        # Crear y agregar la nueva entrada a la tabla de SongPlaylist
+        song_playlist = SongPlaylist(song_id=song_id, playlist_id=playlist_id)
+        db.session.add(song_playlist)
+        db.session.commit()
+
+        return jsonify(song_playlist.to_dict()), 201
+    except Exception as e:
+        return jsonify({'message': f'Error interno del servidor: {str(e)}'}), 500
+
+
+def delete_song_playlist(song_id, playlist_id):
+    try:
+        # Verificar si la canción existe
+        song = Song.query.get(song_id)
+        if not song:
+            return jsonify({'message': 'Canción no encontrada.'}), 404
+
+        # Verificar si la playlist existe
+        playlist = Playlist.query.get(playlist_id)
+        if not playlist:
+            return jsonify({'message': 'Playlist no encontrada.'}), 404
+
+        # Buscar la entrada en SongPlaylist para la canción y la playlist especificadas
+        song_playlist = SongPlaylist.query.filter_by(song_id=song_id, playlist_id=playlist_id).first()
+        if not song_playlist:
+            return jsonify({'message': 'La canción no se encuentra en la playlist.'}), 404
+
+        # Eliminar la entrada de SongPlaylist
+        db.session.delete(song_playlist)
+        db.session.commit()
+
+        return jsonify({'message': 'Canción eliminada de la playlist exitosamente.'}), 200
     except Exception as e:
         return jsonify({'message': f'Error interno del servidor: {str(e)}'}), 500
--- server-python/app/routes/song_playlist_routes.py
+++ server-python/app/routes/song_playlist_routes.py
@@ -16,3 +16,18 @@ def get_songs_playlists_by_user_route():
 @bp.route('/songs/playlists/<int:playlist_id>', methods=['GET'])
 def get_songs_playlist_route(playlist_id):
     return spc.get_songs_playlist(playlist_id)
+
+
+@bp.route('/songs/playlists', methods=['POST'])
+def post_song_playlist_route():
+    return spc.register_song_playlist()
+
+
+# GET /songs/playlists?song_id=value&playlist_id=value
+@bp.route('/songs/playlists', methods=['DELETE'])
+def delete_song_playlist():
+    song_id = request.args.get('song_id')
+    playlist_id = request.args.get('playlist_id')
+    if not song_id or not playlist_id:
+        return jsonify({'message': 'El song_id y el playlist_id son necesarios.'}), 400
+    return spc.delete_song_playlist(song_id, playlist_id)
