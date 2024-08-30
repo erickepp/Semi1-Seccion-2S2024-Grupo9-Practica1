524f4bb81c150b618f99a90c775ae034bf24b399
Erick Patzan <erickestuardopatzan@gmail.com>
2024-08-29 23:45:34 -0600

    Se agregó el endpoint para la verificación de estado

--- server-python/app.py
+++ server-python/app.py
@@ -6,6 +6,10 @@ load_dotenv()
 
 app = create_app()
 
+@app.route('/check', methods=['GET'])
+def check():
+    return '', 200
+
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     app.run(debug=True, port=port)
